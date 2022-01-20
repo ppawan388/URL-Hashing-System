18ceeb4683e732a3613c5c09811cf9d895a1dd4b
ppawan388 <ppawan388@gmail.com>
2022-01-20 18:46:54 +0530

    frontend of url shortner created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 
 const app = express();
 
@@ -9,10 +9,21 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 const PORT = 5000;
+app.use(cors());
+app.use(function (req, res, next) {
 
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+  res.header("Access-Control-Allow-Origin",
+              "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  ,"*");
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE');
+    return res.status(200).json({});
+  }
 
+  next();
+});
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
